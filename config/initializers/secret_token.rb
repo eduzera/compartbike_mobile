3dd6e7471cd330a220bae51e3ba7cb66db514f66
Eduardo Zaghi <eduardozaghi@gmail.com>
2012-11-12 12:54:38 -0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompartbikeMobile::Application.config.secret_token = '9880e9f29c95ab510ce86db00b2d41f0b0239279020a12a27cad5903fbbe97a542dde124b8617dc521d1417d3b13fdfaf9ef00bbce44de6f8a1a3cab69883a7c'
